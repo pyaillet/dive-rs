69c9642fdfc610cac3c519c2d96d922174e7c1f0
Pierre-Yves Aillet <pyaillet@gmail.com>
2020-11-08 16:23:58 +0100

    :recycle: remove useless Result wrapping

--- src/oci/image/mod.rs
+++ src/oci/image/mod.rs
@@ -2,50 +2,46 @@ pub mod config;
 pub mod layer;
 pub mod manifest;
 
-use std::error;
-
 pub trait Reference {
-    fn registry(self) -> Result<String, Box<dyn error::Error>>;
+    fn registry(self) -> String;
 
-    fn fullname(self) -> Result<String, Box<dyn error::Error>>;
+    fn fullname(self) -> String;
 
-    fn tag(self) -> Result<String, Box<dyn error::Error>>;
+    fn tag(self) -> String;
 
-    fn digest(self) -> Result<String, Box<dyn error::Error>>;
+    fn digest(self) -> String;
 }
 
 #[derive(Debug, PartialEq)]
 pub struct ImageReference(String);
 
 impl Reference for ImageReference {
-    fn registry(self) -> Result<String, Box<dyn error::Error>> {
+    fn registry(self) -> String {
         match self.0.find("/") {
-            Some(x) => Ok(self.0[..x].to_string()),
-            None => Ok("docker.io".to_string()),
+            Some(x) => self.0[..x].to_string(),
+            None => "docker.io".to_string(),
         }
     }
 
-    fn fullname(self) -> Result<String, Box<dyn error::Error>> {
+    fn fullname(self) -> String {
         match self.0.find("/") {
-            Some(x) => Ok(self.0[x + 1..].to_string()),
-            None => Ok(self.0),
+            Some(x) => self.0[x + 1..].to_string(),
+            None => self.0,
         }
     }
 
-    fn tag(self) -> Result<String, Box<dyn error::Error>> {
-        match self.0.rfind("/") {
-            Some(x) => match self.0[x..].find(":") {
-                Some(y) => Ok(self.0[x + y + 1..].to_string()),
-                None => Ok("latest".to_string()),
-            },
-            None => Ok("".to_string()),
+    fn tag(self) -> String {
+        let fullname = self.fullname();
+        match fullname.find(":") {
+            Some(x) => fullname[x + 1..].to_string(),
+            None => "latest".to_string(),
         }
     }
 
-    fn digest(self) -> Result<String, Box<dyn error::Error>> {
+    fn digest(self) -> String {
         match self.0.find("@") {
-            Some(x) => Ok(self.0[x..].to_string()),
-            None => Ok("".to_string()),
+            Some(x) => self.0[x..].to_string(),
+            None => "".to_string(),
         }
     }
 }
@@ -57,15 +53,11 @@ mod tests {
     #[test]
     fn tag_ok() {
         assert_eq!(
-            ImageReference("localhost:5000/test/test:v1".to_string())
-                .tag()
-                .unwrap(),
+            ImageReference("localhost:5000/test/test:v1".to_string()).tag(),
             "v1".to_string()
         );
         assert_eq!(
-            ImageReference("localhost:5000/test/test".to_string())
-                .tag()
-                .unwrap(),
+            ImageReference("localhost:5000/test/test".to_string()).tag(),
             "latest".to_string()
         );
     }
@@ -73,26 +65,20 @@ mod tests {
     #[test]
     fn fullname_ok() {
         assert_eq!(
-            ImageReference("localhost:5000/test/test:v1".to_string())
-                .fullname()
-                .unwrap(),
+            ImageReference("localhost:5000/test/test:v1".to_string()).fullname(),
             "test/test:v1".to_string()
         );
         assert_eq!(
-            ImageReference("localhost:5000/test/test".to_string())
-                .fullname()
-                .unwrap(),
+            ImageReference("localhost:5000/test/test".to_string()).fullname(),
             "test/test".to_string()
         );
     }
 
     #[test]
     fn registry_ok() {
-        let image = ImageReference("localhost:5000/myorg/myimage/test:local".to_string());
-
-        let r = image.registry();
-
-        assert!(r.is_ok(), "Manifest parsing failed: `{}`", r.err().unwrap());
-        assert_eq!(r.unwrap(), "localhost:5000");
+        assert_eq!(
+            ImageReference("localhost:5000/myorg/myimage/test:local".to_string()).registry(),
+            "localhost:5000"
+        );
     }
 }
