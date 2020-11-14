0b8218bd649e5da65542593e3b211178018bf6f9
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2020-11-14 07:46:38 +0100

    feat: :sparkles: config fetch & parse (#4)

--- src/oci/image/manifest.rs
+++ src/oci/image/manifest.rs
@@ -28,11 +28,15 @@
 //  }
 //}
 
-use std::collections::HashMap;
+use std::collections::HashMap as Map;
 
 use serde::{Deserialize, Serialize};
 
 use crate::oci::Hash;
+
+pub const MANIFEST_MIME: &str = "application/vnd.docker.distribution.manifest.v2+json";
+pub const CONFIG_MIME: &str = "application/vnd.docker.container.image.v1+json";
+pub const LAYER_MIME: &str = "application/vnd.docker.image.rootfs.diff.tar.gzip";
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "camelCase")]
@@ -50,25 +54,8 @@ pub enum Architecture {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct History {
-    pub v1_compatibility: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct Layer {
     pub blob_sum: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct ManifestV1 {
-    pub schema_version: u8,
-    pub name: String,
-    pub tag: String,
-    pub architecture: Architecture,
-    pub fs_layers: Vec<Layer>,
-    pub history: Vec<History>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -81,11 +68,11 @@ pub struct Media {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ManifestV2 {
+pub struct Manifest {
     pub schema_version: u8,
     pub config: Media,
     pub layers: Vec<Media>,
-    pub annotations: HashMap<String, String>,
+    pub annotations: Option<Map<String, String>>,
 }
 
 #[cfg(test)]
@@ -121,7 +108,7 @@ mod tests {
             }
         }"#;
 
-        let m: Result<ManifestV2, serde_json::Error> = serde_json::from_str(c);
+        let m: Result<Manifest, serde_json::Error> = serde_json::from_str(c);
 
         assert!(m.is_ok(), "Manifest parsing failed: `{}`", m.err().unwrap());
     }
--- src/oci/mod.rs
+++ src/oci/mod.rs
@@ -6,3 +6,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Hash(String);
+
+impl ToString for Hash {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
